0d5cc7adfa8427a28fcb635f8a61d93ff6d42dad
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2025-03-27 14:39:16 -0300

    Update funcoes.js

--- aula2/funcoes.js
+++ aula2/funcoes.js
@@ -5,23 +5,13 @@ var ctxRodape;
 
 function configEstiloCabecalho()
 {
-    bg = document.getElementById("corFundo").value;
-    corFonte = getElementById("corFonte").value;
-    tamFonte = getElementById("tamFonte").value;
-    ctxCabecalho = "#cabecalho{\n background-color:"+bg+";\n";
-    ctxCabecalho += "color:"+corFonte+";\n";
-    ctxCabecalho += "font-size:"+tamFonte+"pt;\n}\n";
-    return ctxCabecalho;
-}
-
-function configEstiloLinks()
-{
-    corLink = document.getElementById("corLinks").value;
-    estiloLinks = document.querySelector('input[name="estiloLinks"]:checked').value;
-    ctxLinks = "a{\n color:"+corLink+";\n";
-    let aux = estiloLinks=="0"?"none":"underline";
-    ctxLinks += "text-decoration:"+aux+";\n}\n";
-    return ctxLinks;
+ bg=document.getElementById("corFundo").value;
+ corFonte=document.getElementById("corFonte").value;
+ tamFonte=document.getElementById("tamFonte").value;
+ ctxCabecalho="#cabecalho{\n background-color:"+bg+";\n";
+ ctxCabecalho+=" color:"+corFonte+";\n";
+ ctxCabecalho+=" font-size:"+tamFonte+"pt;\n}\n";
+ return ctxCabecalho;
 }
 
 function configHTMLCabecalho()
@@ -31,23 +21,32 @@ function configHTMLCabecalho()
     return ctxCabecalho;
 }
 
+function configEstiloLinks()
+{
+    corLink=document.getElementById("corLinks").value;
+    estiloLinks=document.querySelector('input[name="estiloLinks"]:checked').value;
+    ctxLinks="a{\n color:"+corLink+";\n";
+    let aux=estiloLinks=="0"?"none":"underline";
+    ctxLinks+=" text-decoration:"+aux+";\n}\n";
+    return ctxLinks;
+}
+
 function gerarCodigo()
 {
-    //codigo css
-    let codeCSS = document.querySelector("#codeCSS");
-    let css = configEstiloCabecalho();
-    css += configEstiloLinks();
-    codeCSS.value = css;
-    
-    //codigo html
-    let codeHTML = document.querySelector("#codeHTML");
-    ctxHTML = "<html>\n<head>\n" +
-            "<link rel='stylesheet' href = 'estilo.css'>\n" +
-            "<title>Minha Página</title>\n" +
-            "</head>\n<body>" +
-            "<div id='cabecalho'>"+configHTMLCabecalho()+"</div>\n" +
-            "<nav id='links'>\n"+configHTMLLinks()+"\n</nav>\n" +
-            "<div id='conteudo'></div>\n" +
-            "</body>\n</html>";
-    codeHTML.value = ctxHTML;
+    //Cóigo para CSS
+     let codeCSS=document.querySelector("#codeCSS");
+     let css=configEstiloCabecalho();
+     css+=configEstiloLinks();
+     codeCSS.value=css;
+    //Código para HTML
+     let codeHTML=document.querySelector("#codeHTML");
+     ctxHTML="<html>\n<head>\n" +
+         "<link rel='stylesheet' href='estilo.css'>\n"+
+         "<title>Miha página</title>\n"+
+         "</head>\n<body>" +
+         "<div id='cabecalho'>"+configHTMLCabecalho()+"</div>\n" +
+         "<nav id='links'>\n"+configHtmlLinks()+"\n</nav>\n" +
+         "<div id='conteudo'></div>\n" +
+         "</body>\n</html>";
+     codeHTML.value=ctxHTML;
 }
